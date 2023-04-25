821f3afec5784b922d0b8f97a3cc303938bc1e77
cappochinho <edwardprempeh18@gmail.com>
2023-04-25 19:52:59 +0100

    Task 1: re

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,18 +4,11 @@
 
 from flask import Flask, render_template
 from flask_babel import Babel
-
-# from config import Config
+from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
-
-
-class Config(object):
-    """This class configures available languages in the app"""
-
-    LANGUAGES = ["en", "fr"]
 
 
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,7 @@
+"""This is the config file for 1-app.py"""
+
+
+class Config(object):
+    """This class configures available languages in the app"""
+
+    LANGUAGES = ["en", "fr"]
