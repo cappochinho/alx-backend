0b9df304e58287393e76851497faf0d2df008b77
cappochinho <edwardprempeh18@gmail.com>
2023-04-27 09:28:23 -1200

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""First attempt at using Babel"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+class Config(object):
+    """This class configures available languages in the app"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """This is the entry point to the app"""
+
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the locale from the user's preference"""
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
