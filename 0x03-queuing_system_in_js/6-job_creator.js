e36d7aa929eb4dffae60b3f5eb71d9af1f3d98e7
cappochinho <edwardprempeh18@gmail.com>
2023-05-24 19:40:05 +0100

    Task 7 + changes to 6

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,24 +1,17 @@
-import kue from 'kue';
-
-let push_notification_code = kue.createQueue();
+const kue = require('kue')
+  , queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: '+233546938313',
-  message: 'Edward Junior Agyemang-Prempeh',
-}
+  phoneNumber: 'string',
+  message: 'string',
+};
 
-const notificationJob = push_notification_code.createJob(jobData);
-
-notificationJob.on('enqueue', function() {
-  console.log(`Notification job created: ${notificationJob.id}`);
+const job = queue.create('push_notification_code', jobData).save(function (err) {
+  if (!err) console.log(`Notification job created: ${job.id}`);
+  })
+  .on('complete', function () {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', function (errorMessage, doneAttempts) {
+    console.log('Notification job failed');
 });
-
-notificationJob.on('complete', function() {
-  console.log('Notification job completed');
-});
-
-notificationJob.on('failed', function() {
-  console.log('Notification job failed');
-});
-
-notificationJob.save();
